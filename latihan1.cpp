b2b0a5e0a585a7345d6c7d30ae9631a8c8702ecc
Iyuz <71634629+Fairuz205027@users.noreply.github.com>
2022-11-15 19:39:11 +0700

    Add files via upload

---
+++ latihan1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+class MHS
+{
+// Access specifier
+  public:
+// Data Members
+  string MHSname;
+// Member Functions()
+  void printname()
+    {
+      cout << "MHS name is: " << MHSname;
+    }
+};
+
+int main() {
+  // Declare an object of class geeks
+  
+  MHS mhs1;
+  // accessing data member
+    
+  mhs1.MHSname = "Rahmat Budi";
+    
+  // accessing member function
+  mhs1.printname();
+  
+  return 0;
+}
+  
